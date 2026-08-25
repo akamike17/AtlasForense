cdb508b4bc1e6f63d489b3686549f5bc5785fb9a
akamike17 <102251868+akamike17@users.noreply.github.com>
2026-08-25 07:54:59 -0600

    AtlasForense

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AtlasForense.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
